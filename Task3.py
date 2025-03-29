85d4f2559659cbdd613d74e1e66ac3125ed41352
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-03-28 17:06:44 -0800

    Task 3 under Control Structure and Logic

--- Task3.py
+++ Task3.py
@@ -1,5 +1,30 @@
-for i in range(0,15):
+#A simple Python program that checks whether  each number form 1 - 15 is either even or odd
+
+for i in range(1,16):
+
+
     if i % 2 == 0:
-        print("The number is even", i)
+
+            print("The number is even", i)
+
+    else:
+        print("The number is Odd", i)
+
+
+
+# A simple calculator
+
+a = input(int("Enter a number"))
+b = input(int("Enter another number"))
+
+operation = input("Enter an operation +, -, *, /")
+if operation == "+":
+   print(a + b)
+
+elif operation == "-":
+     print(a-b)
+
+elif operation == "*":
+    print(a*b)
 else:
-    print("The number is Odd", i)
+     print(a/b)
