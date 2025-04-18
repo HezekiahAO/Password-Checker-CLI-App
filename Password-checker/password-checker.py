e1fffa5bb5f6753e4bc6966a086768f5d945525b
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-04-18 11:06:44 -0800

    initial commit

--- Password-checker/password-checker.py
+++ Password-checker/password-checker.py
@@ -8,36 +8,35 @@ import string   # This is what allowes me to test for symbols in this code
 
 special_symbol = string.punctuation   
 
-def password_checker(password):
-
-    
-    has_upper = any(char.isupper() for char in password)
-    has_digit = any(char.isdigit() for char in password)
-    has_symbol = any(char in special_symbol for char in password) 
-
-    
-    if len(password) < 8:
-        print("Please enter an 8 digit passeword or more:    ")
-
-    if not any(char.isupper() for char in password) == False :
-        print("Please include at least an upper Case in your password  ")
+has_upper = any(char.isupper() for char in password)
+has_digit = any(char.isdigit() for char in password)
+has_symbol = any(char in special_symbol for char in password) 
 
 
-    if not any(char.isdigit() for char in password) == False :
-        print("Please include at least a digit in your password  ")
+if len(password) < 8:
+    print("Please enter an 8 digit passeword or more:    ")
+
+if not any(char.isupper() for char in password) == False :
+    print("Please include at least an upper Case in your password  ")
 
 
-    if not any(char in special_symbol for char in password) == False :
-        print("Please include a Special Character in your password.   Na for security :) ")
+if not any(char.isdigit() for char in password) == False :
+    print("Please include at least a digit in your password  ")
 
-    else:
-        print("Your Password has been Created!  ")
+
+if not any(char in special_symbol for char in password) == False :
+    print("Please include a Special Character in your password.   Na for security :) ")
+
+else:
+    print("Your Password has been Created!  ")
         
 
 
-    # This aspect of code is specifically testing the strength of the password
-strength = sum([has_upper, has_digit, has_symbol])
-   
+# This aspect of code is specifically testing the strength of the password
+
+
+strength = sum(has_upper, has_digit, has_symbol)
+
 if strength >= 3 and len(password) >=8:
     print("Password Strength is Strong   ")
 
@@ -47,4 +46,4 @@ elif strength == 2 and len(password) >=8:
 else:
     print("Your Password is Weak   ")   
         
-    # This aspect of code is specifically testing the strength of the password
+# This aspect of code is specifically testing the strength of the password
