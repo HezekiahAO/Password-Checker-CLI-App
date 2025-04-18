74c1d0ccd2f8585f820ffcfcfd39539e7ac49163
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-04-17 19:40:50 -0800

    Initial Commit

--- Password-checker/password-checker.py
+++ Password-checker/password-checker.py
@@ -1,17 +1,27 @@
-x = input("Enter your Password:  ")
-print(type(x))
-print(len(x))
+print("Please Before you enter your Password, Ensure to include a symbol, a digit and at least an Upper Case. Thank you")
 
-if len(x) < 8:
+password = input("Enter your Password:  ")
+    # print(type(x))
+    # print(len(x))
+
+import string
+
+special_symbol = string.punctuation
+
+
+if len(password) < 8:
     print("Please enter an 8 digit passeword or more:    ")
-else:
-    print("Lets go to the next step")
 
-if x == x:
-    
-   a = print(x.islower())   
-else:
-    print("")
+elif any(char.isupper() for char in password) == False :
+    print("Please include at least an upper Case in your password  ")
 
-if a == True:
-    print("Enter an Uppercase Letter:  ")
+
+elif any(char.isdigit() for char in password) == False :
+    print("Please include at least a digit in your password  ")
+
+
+elif any(char in special_symbol for char in password) == False :
+    print("Please include a Special Character in your password.   Na for security :) ")
+
+else:
+    print("Your Password has been Created!  ")
