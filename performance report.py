5674a341f935ab2df2b0642caba69c8fb8bcab65
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-04-26 06:50:53 -0800

    full commit

--- performance report.py
+++ performance report.py
@@ -1,4 +1,4 @@
-def get_score():
+def get_scores():
     students = (
         {"name": "Alice", "scores": [70, 80, 90]},
         {"name": "Bob", "scores": [60, 75, 85]},
@@ -12,14 +12,40 @@ def get_score():
             yield student["name"], score  # Gives out both socres and name
 
 # Calling the get_score function 
-for name, score in get_score():
+for name, score in get_scores():
     print(name, score)
 
 
 # Recursive funtion to compute the average score
-def average_score(students):
-    length = len(students)
-    sum = sum(students)
 
-    x = sum("scores")/length("score")
-    print("The Average score of the students is " ,x)
+def get_scores():
+    students = (
+        {"name": "Alice", "scores": [70, 80, 90]},
+        {"name": "Bob", "scores": [60, 75, 85]},
+        {"name": "David", "scores": [50, 23, 90]},
+        {"name": "Hezekiah", "scores": [100, 100, 100]}
+    )
+
+    for student in students:
+        for score in student["scores"]:
+            yield score  # this is the same code form the first but i just yield scores un this to allow me to get an average
+
+
+def average_score(students):
+
+    scores = students[scores] # access the list of scores at the beginning and gets the required value
+
+    length = len(scores) # i used this to get the length of the student scores
+
+    total = sum(scores)  # Sums the total length of the scores
+
+    print("Number of scores:", length)
+
+    average = total / length
+    print("The Average score of the students is " ,average)
+
+for students in get_scores():
+    average_score(students)       # This helps me loop through all the student and then compute their average score
+
+
+   
